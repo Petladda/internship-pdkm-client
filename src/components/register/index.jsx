8f804cfbe7661f43e556c22169d089f4efae1b90
Helio Trope <petladda.94@gmail.com>
2023-05-26 18:12:44 +0700

    fix: add react hook form

--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -2,11 +2,16 @@
 
 import React, { useEffect, useState } from "react";
 import { useLineLiff } from "@/app/context/lineliff.context";
+import Button from "../shared/button";
+import { useForm } from "react-hook-form";
+
 
 const RegisterForm = () =>{
     
     const {liffObject} = useLineLiff() 
     const [profileUser,setProfileUser]= useState()
+    const {register,formState:{errors},handleSubmit} = useForm()
+    
     
     const Login = ()=>{
         if (liffObject.isLoggedIn()) {
@@ -22,6 +27,14 @@ const RegisterForm = () =>{
     }
     console.log("profile",profileUser);
 
+    const submit = ()=>{
+        console.log("submit");
+    }
+
+    const isCkeckbox = ()=>{
+        console.log("ischeck");
+    }
+
     useEffect(()=>{
         if (liffObject) {
             liffObject.ready.then(()=>{
@@ -33,71 +46,76 @@ const RegisterForm = () =>{
 
     return(
         <>
-            <form className="w-80 h-full "> 
-                <div className="flex flex-col">
-                    <label>ชื่อจริง <span className="text-danger">*</span></label>
-                    <input placeholder="ชื่อจริง" className="border"></input>
+            <form onSubmit={handleSubmit(submit)} className="flex flex-col w-96  border mx-auto rounded-lg my-8 shadow-md mb-28"> 
+                <div className="flex flex-col mx-5 my-6 ">
+                    <label className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-2 " htmlFor="Firstname">ชื่อจริง </label>
+                    <input placeholder="ชื่อจริง" className="border rounded-lg  h-12 px-5 " {...register("Firstname",{required: "* กรุณากรอกข้อมูล"})} aria-invalid={errors.Firstname? "true":"false"}></input>
+                    {errors.Firstname && <p  role="alert" className="text-red-500 ">{errors.Firstname?.message}</p>}
 
-                    <label>นามสกุล <span>*</span></label>
-                    <input placeholder="นามสกุล"></input>
+                    <label className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-2 mt-4" htmlFor="Lastname">นามสกุล </label>
+                    <input placeholder="นามสกุล" className="border rounded-lg  h-12 px-5 "  {...register("Lastname",{required: "* กรุณากรอกข้อมูล"})} aria-invalid={errors.Lastname? "true":"false"}></input>
+                    {errors.Lastname && <p  role="alert" className="text-red-500 ">{errors.Lastname?.message}</p>}
 
-                    <label>ชื่อเล่น <span>*</span></label>
-                    <input placeholder="ชื่อเล่น"></input>
-                    <div>
-                        <label>เพศ <span>*</span></label>
-                        <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white  rounded-lg sm:flex dark:text-white">
-                            <li class="w-full sm:border-b-0 sm:border-r">
-                                <div class="flex items-center pl-3">
-                                    <input id="vue-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2"></input>
-                                    <label for="vue-checkbox-list" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 ">หญิง</label>
-                                </div>
-                            </li>
-                            <li class="w-full  sm:border-b-0 sm:border-r">
-                                <div class="flex items-center pl-3">
-                                    <input id="react-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2"></input>
-                                    <label for="react-checkbox-list" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 ">ชาย</label>
-                                </div>
-                            </li>
-                            <li class="w-full  sm:border-b-0 sm:border-r">
-                                <div class="flex items-center pl-3">
-                                    <input id="angular-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 "></input>
-                                    <label for="angular-checkbox-list" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 ">ไม่ระบุ</label>
-                                </div>
-                            </li>
-                        </ul>
-
-                    </div>
-
-                    <div>
-
-                    </div>
-                    <label>ทีม</label>
-                    <select>
-                        <option selected >select team</option>
-                        <option>deploy</option>
-                        <option>deploy</option>
-                        <option>deploy</option>
-                    </select>
+                    <label className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-2 mt-4" htmlFor="Nickname">ชื่อเล่น </label>
+                    <input placeholder="ชื่อเล่น"  className="border rounded-lg  h-12 px-5 "  {...register("Nickname",{required: "* กรุณากรอกข้อมูล"})} aria-invalid={errors.Nickname? "true":"false"}></input>
+                    {errors.Nickname && <p  role="alert" className="text-red-500 ">{errors.Nickname?.message}</p>}
                     
-                    <label>ทีม</label>
-                    <select>
-                        <option selected>select</option>
-                        <option>UI</option>
-                        <option>UI</option>
-                        <option>UI</option>
-                    </select>
-                    
-                    <div class="flex items-center mb-4">
-                        <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700"></input>
-                        <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 ">
-                            ฉันได้อ่านและยอมรับเงื่อนไขการเป็นสมาชิกและนโยบายความเป็นส่วนตัว <span>*</span></label>
+                    <div className="mt-4">
+                        <label className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-2 " htmlFor="Sex">เพศ </label>
+                        <fieldset className="flex flex-row mb-4 justify-evenly ">
+                            <input id="women"  type="radio" name="status" className="accent-green-600 " checked />
+                            <label for="women" className=" ">หญิง</label>
+
+                            <input id="men" className="accent-green-600" type="radio" name="status" />
+                            <label for="men" className="">ชาย</label>
+
+                            <input id="not-specified" className="accent-green-600" type="radio" name="status" />
+                            <label for="not-specified" className="">ไม่ระบุ</label>
+
+                        </fieldset>
+                        
                     </div>
 
-                    <button>ยืนยัน</button>
+                    <div >
+                        <p className="mb-3">ทีม</p>
+                        <select className="border  rounded-lg w-full h-12 px-5  ">
+                            
+                            <option selected>Paiduay</option>
+                            <option>Deeploy</option>
+                            <option>Ourpoint</option>
+                            <option>Nextate</option>
+                            <option>Daywork</option>         
+                        </select>
+ 
+                    </div>
+                    <div >
+                        <p className="mb-3 mt-4">ตำแหน่ง</p>
+                        <select className="border rounded-lg w-full  h-12 px-5 my-">
+                            <option>UI</option>
+                            <option>UI</option>
+                            <option>UI</option>
+                        </select>
+ 
+                    </div>
+                    <div className="border-b-2 mt-10 border-extar-light-grey "></div>
+                    <div class="flex items-center mb-4 mt-4 ">
+                        <input type="checkbox" className="accent-black" {...register("checkbox",{required: "* กรุณากรอกข้อมูล"})} aria-checked={errors.checkbox? "true":"false"} />
+                        <label for="default-checkbox" className="ml-2 text-sm font-medium text-mid-grey after:content-['*'] after:ml-0.5 after:text-red-500 ">
+                                ฉันได้อ่านและยอมรับเงื่อนไขการเป็นสมาชิกและนโยบายความเป็นส่วนตัว </label>
 
-                    <h5>การสร้างบัญชีหรือการเข้าใช้งาน หมายถึงคุณได้อ่านและยอมรับ</h5>
-                    <h5>เงื่อนไขข้อกำหนด และ นโยบายความเป็นส่วนตัว</h5>             
+                    </div>
+                        {errors.checkbox && <p  role="alert" className=" text-red-500 mb-2 pl-3 ">{errors.checkbox?.message}</p>}
+                   
+                   <Button/>
+                    <div className="font-medium text-center mb-5">
+                        <p className="mt-4  text-mid-grey">การสร้างบัญชีหรือการเข้าใช้งาน หมายถึงคุณได้อ่านและยอมรับ</p>
+                        <p>เงื่อนไขข้อกำหนด <span className="text-mid-grey">และ</span> นโยบายความเป็นส่วนตัว</p>
+                    </div>
+                
+
                 </div>
+                    
+                
             </form>
         </>
     )
---
+++ src/components/shared/button/index.jsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+const Button = ()=>{
+    return(
+        <button type="submit" className="border rounded-xl bg-secondary h-12 text-white ">ยืนยัน</button>
+    )
+}
+
+export default Button;

b91e84f41a8ba7581670c31b446fa01b0ee81c42
Helio Trope <petladda.94@gmail.com>
2023-05-29 17:56:24 +0700

    fix: add react-select for option

--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -4,109 +4,177 @@ import React, { useEffect, useState } from "react";
 import { useLineLiff } from "@/app/context/lineliff.context";
 import Button from "../shared/button";
 import { useForm } from "react-hook-form";
-
+import Select from "react-select"
 
 const RegisterForm = () =>{
     
     const {liffObject} = useLineLiff() 
     const [profileUser,setProfileUser]= useState()
-    const {register,formState:{errors},handleSubmit} = useForm()
+    const {register,formState:{errors},handleSubmit,setValue,getValues,clearErrors,reset} = useForm()
     
-    
-    const Login = ()=>{
+    const team_id = [
+        { value: '1', label: 'PDKM' },
+        { value: '2', label: 'DAY WORK' },
+        { value: '3', label: 'DEEPLOY' },
+        { value: '4', label: 'NEST8' },
+        { value: '5', label: 'OURPOINT' },
+    ]
+
+    const position_id = [
+        { value: '1', label: 'CEO' },
+        { value: '2', label: 'Head of Mobile Developer' },
+        { value: '3', label: 'Mobile Developer' },
+        { value: '4', label: 'Font-end Developer' },
+        { value: '5', label: 'Junior Font-end Developer' },
+        { value: '6', label: 'Quality Assurance' },
+        { value: '7', label: 'Junior Quality Assurance' },
+        { value: '8', label: 'Product Manager' },
+        { value: '9', label: 'Back-end Developer' },
+        { value: '10', label: 'Junior Back-end Develope' },
+        { value: '11', label: 'UX Designer' },
+        { value: '12', label: 'UI Designer' },
+        { value: '13', label: 'Business Analyst' },
+        { value: '14', label: 'Managing Director' },
+        { value: '15', label: 'Full-Stack Developer' },
+        { value: '16', label: 'Junior Full-Stack Developer' },
+        { value: '17', label: 'Business Developer' },
+        { value: '18', label: 'Customer Support' },
+        { value: '19', label: 'PHP Developer' },
+        { value: '20', label: 'Tecnical Director' },
+        { value: '21', label: 'General Manager' },
+        { value: '22', label: 'Accounting Administrative Assistant' },
+        { value: '23', label: 'แม่บ้าน' },
+
+    ]
+
+
+
+    const login = ()=>{
         if (liffObject.isLoggedIn()) {
-            console.log("login success");
+            console.log("login success!!!");
         }else{
-            // liffObject.login()
+            liffObject.login()
         }
     }
     
-    const Profile = async()=>{
+    const profile = async()=>{
         const profileUser = await liffObject.getProfile()
         setProfileUser(profileUser)
     }
-    console.log("profile",profileUser);
+    // console.log("profileUser:" ,profileUser);
 
-    const submit = ()=>{
-        console.log("submit");
+
+    const isSubmit = (data)=>{
+        console.log("isdisabled",data);
+        
     }
 
-    const isCkeckbox = ()=>{
-        console.log("ischeck");
+    const sexTypeSelect = (e)=>{
+        setValue('sex',e.target.value)
+        clearErrors("sex")
+    }
+
+    const handleSelectTeam = (e)=>{
+        setValue('team_id',e.value) 
+        
+    }
+    
+    const handleSelectPosition = (e)=>{
+        setValue('position_id',e.value) 
+
     }
 
     useEffect(()=>{
         if (liffObject) {
             liffObject.ready.then(()=>{
-                Login()
-                Profile()
+                login()
+                profile()
             })
         }
     },[liffObject])
 
+    useEffect(()=>{
+        if (profileUser) {
+            reset({
+                user_line_id: profileUser.userId
+            })
+        }
+    },[profileUser])
+
     return(
         <>
-            <form onSubmit={handleSubmit(submit)} className="flex flex-col w-96  border mx-auto rounded-lg my-8 shadow-md mb-28"> 
+            <form onSubmit={handleSubmit(isSubmit)} className="flex flex-col w-96  border mx-auto rounded-lg my-8 shadow-md mb-28"> 
                 <div className="flex flex-col mx-5 my-6 ">
-                    <label className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-2 " htmlFor="Firstname">ชื่อจริง </label>
-                    <input placeholder="ชื่อจริง" className="border rounded-lg  h-12 px-5 " {...register("Firstname",{required: "* กรุณากรอกข้อมูล"})} aria-invalid={errors.Firstname? "true":"false"}></input>
-                    {errors.Firstname && <p  role="alert" className="text-red-500 ">{errors.Firstname?.message}</p>}
+                    <label className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-2 ">ชื่อจริง </label>
+                    <input placeholder="ชื่อจริง" className="border rounded-lg  h-12 px-5 " {...register("first_name",{required: "* กรุณากรอกข้อมูล"})} aria-invalid={errors.first_name? "true":"false"}></input>
+                    {errors.first_name && <p  role="alert" className="text-red-500 ">{errors.first_name?.message}</p>}
 
-                    <label className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-2 mt-4" htmlFor="Lastname">นามสกุล </label>
-                    <input placeholder="นามสกุล" className="border rounded-lg  h-12 px-5 "  {...register("Lastname",{required: "* กรุณากรอกข้อมูล"})} aria-invalid={errors.Lastname? "true":"false"}></input>
-                    {errors.Lastname && <p  role="alert" className="text-red-500 ">{errors.Lastname?.message}</p>}
+                    <label className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-2 mt-4" >นามสกุล </label>
+                    <input placeholder="นามสกุล" className="border rounded-lg  h-12 px-5 "  {...register("last_name",{required: "* กรุณากรอกข้อมูล"})} aria-invalid={errors.last_name? "true":"false"}></input>
+                    {errors.last_name && <p  role="alert" className="text-red-500 ">{errors.last_name?.message}</p>}
 
-                    <label className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-2 mt-4" htmlFor="Nickname">ชื่อเล่น </label>
-                    <input placeholder="ชื่อเล่น"  className="border rounded-lg  h-12 px-5 "  {...register("Nickname",{required: "* กรุณากรอกข้อมูล"})} aria-invalid={errors.Nickname? "true":"false"}></input>
-                    {errors.Nickname && <p  role="alert" className="text-red-500 ">{errors.Nickname?.message}</p>}
+                    <label className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-2 mt-4" >ชื่อเล่น </label>
+                    <input placeholder="ชื่อเล่น"  className="border rounded-lg  h-12 px-5 "  {...register("nickname",{required: "* กรุณากรอกข้อมูล"})} aria-invalid={errors.nickname? "true":"false"}></input>
+                    {errors.nickname && <p  role="alert" className="text-red-500 ">{errors.nickname?.message}</p>}
                     
                     <div className="mt-4">
-                        <label className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-2 " htmlFor="Sex">เพศ </label>
+                        <label className="after:content-['*'] after:ml-0.5 after:text-red-500 mb-2 ">เพศ </label>
                         <fieldset className="flex flex-row mb-4 justify-evenly ">
-                            <input id="women"  type="radio" name="status" className="accent-green-600 " checked />
-                            <label for="women" className=" ">หญิง</label>
+                            <input id="female"  type="radio" name="status" className="accent-green-600 "
+                            {...register("sex",{required: "* กรุณากรอกข้อมูล"})} aria-checked={errors.sex? "true":"false"}
+                            value="หญิง"
+                            checked={getValues("value")}
+                            onChange={sexTypeSelect}                          
+                            />
+                            <label htmlFor="female" >หญิง</label>
 
-                            <input id="men" className="accent-green-600" type="radio" name="status" />
-                            <label for="men" className="">ชาย</label>
+                            <input id="male" className="accent-green-600" type="radio" name="status" 
+                            {...register("sex",{required: "* กรุณากรอกข้อมูล"})} aria-checked={errors.sex? "true":"false"}
+                            value="ชาย"
+                            checked={getValues("value")}
+                            onChange={sexTypeSelect}
+                            />
+                            <label htmlFor="male" >ชาย</label>
 
-                            <input id="not-specified" className="accent-green-600" type="radio" name="status" />
-                            <label for="not-specified" className="">ไม่ระบุ</label>
+                            <input id="none" className="accent-green-600" type="radio" name="status" 
+                            {...register("sex",{required: "* กรุณากรอกข้อมูล"})} aria-checked={errors.sex? "true":"false"}
+                            value="ไม่ระบุ"
+                            checked={getValues("value")}
+                            onChange={sexTypeSelect}
+                            />
+                            <label htmlFor="none">ไม่ระบุ</label>
 
                         </fieldset>
+                        {errors.sex && <p  role="alert" className="text-red-500 ">{errors.sex?.message}</p>}
+
                         
                     </div>
 
-                    <div >
-                        <p className="mb-3">ทีม</p>
-                        <select className="border  rounded-lg w-full h-12 px-5  ">
-                            
-                            <option selected>Paiduay</option>
-                            <option>Deeploy</option>
-                            <option>Ourpoint</option>
-                            <option>Nextate</option>
-                            <option>Daywork</option>         
-                        </select>
- 
+                    <div  >
+                        <p className="mb-3">ทีม</p>                     
+                            <Select 
+                            options={team_id}
+                            checked={getValues()}
+                            onChange={handleSelectTeam}  />        
                     </div>
                     <div >
                         <p className="mb-3 mt-4">ตำแหน่ง</p>
-                        <select className="border rounded-lg w-full  h-12 px-5 ">
-                            <option>UI</option>
-                            <option>UI</option>
-                            <option>UI</option>
-                        </select>
+                        <Select 
+                        options={position_id}
+                        checked={getValues()}
+                        onChange={handleSelectPosition} />
  
                     </div>
                     <div className="border-b-2 mt-10 border-extar-light-grey "></div>
-                    <div class="flex items-center mb-4 mt-4 ">
+                    <div className="flex items-center mb-4 mt-4 ">
                         <input type="checkbox" className="accent-black" {...register("checkbox",{required: "* กรุณากรอกข้อมูล"})} aria-checked={errors.checkbox? "true":"false"} />
-                        <label for="default-checkbox" className="ml-2 text-sm font-medium text-mid-grey after:content-['*'] after:ml-0.5 after:text-red-500 ">
+                        <label htmlFor="checkbox" className="ml-2 text-sm font-medium text-mid-grey after:content-['*'] after:ml-0.5 after:text-red-500 ">
                                 ฉันได้อ่านและยอมรับเงื่อนไขการเป็นสมาชิกและนโยบายความเป็นส่วนตัว </label>
 
                     </div>
                         {errors.checkbox && <p  role="alert" className=" text-red-500 mb-2 pl-3 ">{errors.checkbox?.message}</p>}
                    
-                   <Button disabled={true} />
+                   <Button disabled={errors.checkbox} isSubmit={isSubmit} title="ยืนยัน" color="secondary"/>
                     <div className="font-medium text-center mb-5">
                         <p className="mt-4  text-mid-grey">การสร้างบัญชีหรือการเข้าใช้งาน หมายถึงคุณได้อ่านและยอมรับ</p>
                         <p>เงื่อนไขข้อกำหนด <span className="text-mid-grey">และ</span> นโยบายความเป็นส่วนตัว</p>
--- src/components/shared/button/index.jsx
+++ src/components/shared/button/index.jsx
@@ -1,18 +1,17 @@
 "use client";
-import React from "react";
 
 
-const Button = ({disabled})=>{
+const Button = ({disabled,isSubmit,title,color})=>{
 
-    const isSubmit = ()=>{
-       disabled = false
-        
-    }
-    
-    console.log("setdisable");
-    
     return(
-        <button disabled={isSubmit}  type="submit" className="border rounded-xl bg-secondary h-12 text-white ">ยืนยัน</button>
+        <>
+        <button disabled={disabled} onClick={isSubmit} type="submit" 
+        className={`border rounded-xl bg-${color} h-12 text-white disabled:bg-light-grey`}>{title}</button>
+
+        
+        </>
+        
+
     )
 }
 
